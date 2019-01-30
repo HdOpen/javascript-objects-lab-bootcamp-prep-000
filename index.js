96fdaaf0aba75503336f6a0db309f5b73c56a00d
Florin <furtuna.florin90@gmail.com>
2019-01-30 14:23:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,4 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, { [key]: value }) ;
-
 }
